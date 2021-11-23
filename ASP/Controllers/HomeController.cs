b517eef7237a7fef24046c34d979222b6475b8d5
Bogdan <bgroholsky@gmail.com>
2021-11-23 22:32:14 +0200

    Обеспечить выбор языков для перевода выбором из выпадающего списка
    Перенести информацию о регионе (global)
    в ApiKey, получать данные асинхронно
    
    * обработка ошибок - если Azure возвращает ошибку,
      выводить соотв. сообщение
    
    ** выводить перевод при выделении текста

--- ASP/Controllers/HomeController.cs
+++ ASP/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Json;
-using Newtonsoft.Json.Linq;
+using System.Net.Http;
 
 namespace DevOpsBasics.Controllers
 {
@@ -17,37 +17,38 @@ namespace DevOpsBasics.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly string path = "./comments.txt";
 
+        private List<Models.Opinion> opinions;
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            opinions = null;
         }
 
         public IActionResult Index()
         {
-            List<string> comments;
-            try
+            if (opinions == null)
             {
-                comments = new List<string>();
-
-                using var reader = new StreamReader(path);
-                var opinions = JsonSerializer
-                    .Deserialize<List<Models.Opinion>>
-                    (reader.ReadToEnd());
-
-                ViewData["CommentsObjects"] = opinions;
-                
-                foreach (var specificOpinions in opinions)
+                try
                 {
-                    comments.Add(specificOpinions.Comment);
+                    using var reader = new StreamReader(path);
+                    opinions = JsonSerializer
+                        .Deserialize<List<Models.Opinion>>
+                        (reader.ReadToEnd());
+
+                    ViewData["Mode"] = "Read";
+                }
+                catch
+                {
+                    opinions = null;
                 }
             }
-
-            catch
+            else
             {
-                comments = null;
+                ViewData["Mode"] = "Repeat";
             }
-            ViewData["Comments"] = comments?.ToArray();
-           
+            ViewData["Comments"] = opinions?.ToArray();
+
             return View();
         }
 
@@ -56,24 +57,97 @@ namespace DevOpsBasics.Controllers
             return View();
         }
 
+        public IActionResult Translate()
+        {
+            String translateFrom = Request.Query["originLang"].ToString();
+            if (String.IsNullOrEmpty(translateFrom))
+            {
+                translateFrom = "en";
+            }
+            String translateTo = Request.Query["translateTo"].ToString();
+            if (String.IsNullOrEmpty(translateTo))
+            {
+                translateTo = "uk";
+            }
+
+            String endpoint = @"https://api.cognitive.microsofttranslator.com";
+            String key = "8eeadf9d8a7040f3920fa56c6abbff1b";
+            String path = $"/translate?api-version=3.0&from={translateFrom}&to={translateTo}";
+            String region = "global";
+            String txt = Request.Query["txt"].ToString();
+            if(txt.Length == 0)
+            {
+                txt = "Hello, world!";
+            }
+            String body = JsonSerializer.Serialize(
+                new object[] { new { Text = txt } });
+            String resp;
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(endpoint + path);
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", region);
+                
+                // request.Headers.Add("Content-Type", "application/json; charset=UTF-8");
+                // request.Headers.Add("Content-Length", body.Length.ToString());
+                request.Content = new StringContent(
+                    body, System.Text.Encoding.UTF8, "application/json");
+
+                resp = client
+                    .SendAsync(request).Result
+                    .Content.ReadAsStringAsync().Result;
+            }
+            ViewData["resp"] = resp;
+            // [{"translations":[{"text":"Привіт, народ!","to":"uk"},{"text":"Всем привет!","to":"ru"}]}]
+            var json = JsonSerializer
+                .Deserialize<Translations[]>(resp);
+            ViewData["ukTranslation"] = "";
+            ViewData["ruTranslation"] = "";
+            
+            foreach (var obj in json)
+            {
+                foreach(var trans in obj.translations)
+                {
+                    if (trans.to.Equals("uk"))
+                        ViewData["ukTranslation"] = trans.text;
+                    if (trans.to.Equals("ru"))
+                        ViewData["ruTranslation"] = trans.text;
+                }
+            }
+            return View();
+        }
+
+        public IActionResult ApiKey()
+        {
+            return Content("{ \"Key\": \"8eeadf9d8a7040f3920fa56c6abbff1b\"," +
+                              "\"Region\": \"global\" }");
+        }
+
         [HttpPost]
         public IActionResult UserOpinion(Models.Opinion opinion)
         {
-            List<Models.Opinion> opinions;
-            try
+            if (opinions == null)
             {
-                using var reader = new StreamReader(path);
-                opinions = JsonSerializer
-                    .Deserialize<List<Models.Opinion>>
-                    (reader.ReadToEnd());
+                ViewData["Mode"] = "Read";
+                try
+                {
+                    using var reader = new StreamReader(path);
+                    opinions = JsonSerializer
+                        .Deserialize<List<Models.Opinion>>
+                        (reader.ReadToEnd());
+                }
+                catch
+                {
+                    opinions = new List<Models.Opinion>();
+                }
             }
-            catch
+            else
             {
-                opinions = new List<Models.Opinion>();              
+                ViewData["Mode"] = "Repeat";
             }
-            opinion.Moment = DateTime.Now;
-       
-          
             opinions.Add(opinion);
             // Serialization
             string comment = JsonSerializer.Serialize(opinions);
@@ -92,7 +166,15 @@ namespace DevOpsBasics.Controllers
         }
     }
 
-
+    class Translation
+    {
+        public String text { get; set; }
+        public String to { get; set; }
+    }
+    class Translations
+    {
+        public Translation[] translations { get; set; }
+    }
 }
 /*
  * XML  <data>10</data>
--- ASP/Models/Opinion.cs
+++ ASP/Models/Opinion.cs
@@ -10,27 +10,5 @@ namespace DevOpsBasics.Models
         public String Usernik { get; set; }
 
         public String Comment { get; set; }
-
-        public String Authorname { get; set; }
-
-        public DateTime Moment { get; set; } 
-
-        public string GetOpinionInfo(string dateCompare = null)
-        {           
-            var yesterday = DateTime.Now.Date.AddDays(-1);
-            var today = DateTime.Now.Date;
-
-
-            if (DateTime.Compare(yesterday, Moment.Date ) == 0)
-            {
-                return $"вчера: {Authorname} \"{Comment}\" ";
-            }
-            else if (DateTime.Compare(today, Moment.Date) == 0)
-            {
-                return $"{Moment.ToShortTimeString()}: {Authorname} \"{Comment}\" ";        
-            }
-            return $"{Moment} {Authorname} \"{Comment}\" ";
-           
-        }
     }
 }
